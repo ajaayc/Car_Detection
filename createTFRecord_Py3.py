850709bc3011cab0b4fdb8a867e076ed8301ce17
Ajaay Chandrasekaran <ajaay@umich.edu>
2017-12-12 19:15:59 -0500

    Fixed merge conflict for real in TFRecord creation

--- createTFRecord_Py3.py
+++ createTFRecord_Py3.py
@@ -5,6 +5,7 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 import numpy as np
 from glob import glob
+import sys
 
 from object_detection.utils import dataset_util
 
@@ -47,15 +48,16 @@ def get_bbox(p0, p1):
 
     return v, e
 
-def create_tf_example(example,counter):
+def create_tf_example(example,counter,datatype):
   print(example, ": " ,counter)
   img = plt.imread(example)
   
   proj = np.fromfile(example.replace('_image.jpg', '_proj.bin'), dtype=np.float32)
   proj.resize([3, 4])
   
-  bbox = np.fromfile(example.replace('_image.jpg', '_bbox.bin'), dtype=np.float32)
-  bbox.resize([bbox.size // 11, 11])
+  if datatype == 'train':
+      bbox = np.fromfile(example.replace('_image.jpg', '_bbox.bin'), dtype=np.float32)
+      bbox.resize([bbox.size // 11, 11])
   
   # TODO(user): Populate the following variables from your example.
   #-----------------------------------------------------------------
@@ -67,7 +69,7 @@ def create_tf_example(example,counter):
 
   #See https://github.com/swirlingsand/deeper-traffic-lights/blob/master/data_conversion_bosch.py for example of how to read in a file
   with tf.gfile.GFile(filename, 'rb') as fid:
-          encoded_image_data = fid.read()
+      encoded_image_data = fid.read()
   
   image_format = 'jpg'.encode() # b'jpg' or b'png'
 
@@ -82,75 +84,108 @@ def create_tf_example(example,counter):
   
   #-----------------------------------------------------------------
   
-  for k, b in enumerate(bbox):
-        ignore_in_eval = bool(b[10])
-        if not ignore_in_eval:
-            n = b[0:3]
-            theta = np.linalg.norm(n)
-            n /= theta
-            R = rot(n, theta)
-            t = b[3:6]
-        
-            sz = b[6:9]
-            vert_3D, edges = get_bbox(-sz / 2, sz / 2)
-            vert_3D = np.dot(R,vert_3D) + t[:, np.newaxis]
-        
-            vert_2D = np.dot(proj,np.vstack([vert_3D, np.ones(8)]))
-            vert_2D = vert_2D / vert_2D[2, :]
+  if datatype == 'train':
+      for k, b in enumerate(bbox):
+            ignore_in_eval = bool(b[10])
+            if not ignore_in_eval:
+                n = b[0:3]
+                theta = np.linalg.norm(n)
+                n /= theta
+                R = rot(n, theta)
+                t = b[3:6]
+
+                sz = b[6:9]
+                vert_3D, edges = get_bbox(-sz / 2, sz / 2)
+                vert_3D = np.dot(R,vert_3D) + t[:, np.newaxis]
+            
+                vert_2D = np.dot(proj,np.vstack([vert_3D, np.ones(8)]))
+                vert_2D = vert_2D / vert_2D[2, :]
+                    
+                #Divided by width and height since train.py requires these are normalized
+                #Don't do integer division
+                xmin = min(vert_2D[0]) / float(width)
+                xmax = max(vert_2D[0]) / float(width)
+                 
+                ymin = min(vert_2D[1]) / float(height)
+                ymax = max(vert_2D[1]) / float(height)
                 
-            #Divided by width and height since train.py requires these are normalized
-            #Don't do integer division
-            xmin = min(vert_2D[0]) / float(width)
-            xmax = max(vert_2D[0]) / float(width)
-             
-            ymin = min(vert_2D[1]) / float(height)
-            ymax = max(vert_2D[1]) / float(height)
-            
-            
-            #xmax = min(xmax, 1.01)
-            #ymax = min(ymax, 1.01)
-            
-            xmins.append(xmin)
-            xmaxs.append(xmax)
-            ymins.append(ymin)
-            ymaxs.append(ymax)
-            
-            class_int = int(b[9])
-            class_text = str.encode(class_strings[class_int])
-    
-            classes.append(class_int)
-            classes_text.append(class_text)
+                
+                xmax = min(xmax, 1.01)
+                ymax = min(ymax, 1.01)
+                
+                xmins.append(xmin)
+                xmaxs.append(xmax)
+                ymins.append(ymin)
+                ymaxs.append(ymax)
+                
+                class_int = int(b[9])
+                class_text = str.encode(class_strings[class_int])
+        
+                classes.append(class_int)
+                classes_text.append(class_text)
         
 
-  tf_example = tf.train.Example(features=tf.train.Features(feature={
-      'image/height': dataset_util.int64_feature(height),
-      'image/width': dataset_util.int64_feature(width),
-      'image/filename': dataset_util.bytes_feature(filename),
-      'image/source_id': dataset_util.bytes_feature(filename),
-      'image/encoded': dataset_util.bytes_feature(encoded_image_data),
-      'image/format': dataset_util.bytes_feature(image_format),
-      'image/object/bbox/xmin': dataset_util.float_list_feature(xmins),
-      'image/object/bbox/xmax': dataset_util.float_list_feature(xmaxs),
-      'image/object/bbox/ymin': dataset_util.float_list_feature(ymins),
-      'image/object/bbox/ymax': dataset_util.float_list_feature(ymaxs),
-      'image/object/class/text': dataset_util.bytes_list_feature(classes_text),
-      'image/object/class/label': dataset_util.int64_list_feature(classes),
-  }))  
+  tf_example = ''
+      
+  if datatype == 'train':
+      tf_example = tf.train.Example(features=tf.train.Features(feature={
+          'image/height': dataset_util.int64_feature(height),
+          'image/width': dataset_util.int64_feature(width),
+          'image/filename': dataset_util.bytes_feature(filename),
+          'image/source_id': dataset_util.bytes_feature(filename),
+          'image/encoded': dataset_util.bytes_feature(encoded_image_data),
+          'image/format': dataset_util.bytes_feature(image_format),
+          'image/object/bbox/xmin': dataset_util.float_list_feature(xmins),
+          'image/object/bbox/xmax': dataset_util.float_list_feature(xmaxs),
+          'image/object/bbox/ymin': dataset_util.float_list_feature(ymins),
+          'image/object/bbox/ymax': dataset_util.float_list_feature(ymaxs),
+          'image/object/class/text': dataset_util.bytes_list_feature(classes_text),
+          'image/object/class/label': dataset_util.int64_list_feature(classes),
+      }))
+
+  if datatype == 'test':
+    tf_example = tf.train.Example(features=tf.train.Features(feature={
+          'image/height': dataset_util.int64_feature(height),
+          'image/width': dataset_util.int64_feature(width),
+          'image/filename': dataset_util.bytes_feature(filename),
+          'image/source_id': dataset_util.bytes_feature(filename),
+          'image/encoded': dataset_util.bytes_feature(encoded_image_data),
+          'image/format': dataset_util.bytes_feature(image_format),
+          #'image/object/bbox/xmin': dataset_util.float_list_feature(xmins),
+          #'image/object/bbox/xmax': dataset_util.float_list_feature(xmaxs),
+          #'image/object/bbox/ymin': dataset_util.float_list_feature(ymins),
+          #'image/object/bbox/ymax': dataset_util.float_list_feature(ymaxs),
+          #'image/object/class/text': dataset_util.bytes_list_feature(classes_text),
+          #'image/object/class/label': dataset_util.int64_list_feature(classes),
+      }))
 
   return tf_example
 
 
 def main(_):
-  #writer = tf.python_io.TFRecordWriter(FLAGS.output_path)
-  writer = tf.python_io.TFRecordWriter("train.record")
+  if len(sys.argv) != 2 or not(sys.argv[1] == 'test' or sys.argv[1] == 'train'):
+      print('Error: Incorrect number of arguments')
+      print('Usage: python createTFRecord_Py3.py <test OR train>')
+      exit
 
+  #Type of the data. 'test' or 'train'
+  datatype = sys.argv[1]
+
+  #writer = tf.python_io.TFRecordWriter(FLAGS.output_path)
+    
   # TODO(user): Write code to read in your dataset to examples variable
-  examples = glob('deploy/trainval/*/*_image.jpg')
-  
+  if datatype == 'train':
+      writer = tf.python_io.TFRecordWriter("train.record")
+      examples = glob('deploy/trainval/*/*_image.jpg')
+
+  if datatype == 'test':
+      writer = tf.python_io.TFRecordWriter("test.record")
+      examples = glob('deploy/test/*/*_image.jpg')
+
   counter = 1
 
   for example in examples:
-    tf_example = create_tf_example(example,counter)
+    tf_example = create_tf_example(example,counter,datatype)
     counter += 1
     writer.write(tf_example.SerializeToString())
 
